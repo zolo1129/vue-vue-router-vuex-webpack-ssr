a3ce61b04a079e07671382b26877fba48c183907
zolo1129 <zolo1129@163.com>
2019-01-04 12:03:47 +0800

    create

---
+++ build/webpack.config.base.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+const config = {
+  target: 'web',
+  entry: path.join(__dirname, '../src/index.js'),
+  output: {
+    filename: 'bundle.[hash:8].js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-plain-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: 'resources/[path][name].[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config
